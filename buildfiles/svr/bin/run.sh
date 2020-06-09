137110c4520ed8714a870e1d2fd06d0060410374
Minamoto Xu <nicai@unknow.com>
2020-06-09 11:00:44 +0800

    fix mysqld

--- buildfiles/svr/bin/run.sh
+++ buildfiles/svr/bin/run.sh
@@ -13,12 +13,12 @@ service ssh restart
 sleep 1
 
 sed -i "s/# requirepass foobared/requirepass arbalest/g" /etc/redis/redis.conf
-service redis-server start
+service redis-server restart
 sleep 1
 
 echo "Starting mysql"
 # service mysql start
-start-stop-daemon --start --name mysqld --background --exec /usr/sbin/mysqld -- -name luwak --data-dir /root/etcd/luwak.etcd --cert-file=/opt/ca/server.pem --key-file=/opt/ca/server-key.pem --advertise-client-urls=https://0.0.0.0:2378,http://0.0.0.0:2379 --listen-client-urls=https://0.0.0.0:2378,http://0.0.0.0:2379
+start-stop-daemon --start --name mysqld --background --exec /usr/sbin/mysqld -- --defaults-file=/root/mariadb/my.cnf
 #start-stop-daemon --start --name mysqld --background --exec /usr/sbin/mysqld -- --bind-address=0.0.0.0 --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/x86_64-linux-gnu/mariadb18/plugin --user=mysql --skip-log-error --pid-file=/tmp/mysqld.pid --socket=/var/lib/mysql/mysqld.sock --port=3306
 sleep 1
 
