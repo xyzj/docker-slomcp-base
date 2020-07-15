1b3f97fbb95ccb81b9f524faa2de2623d47041e1
Minamoto Xu <nicai@unknow.com>
2020-07-15 09:44:58 +0800

    更新mysql配置文件

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -dti  --privileged --restart=on-failure -p 80:80 -p 443:443 -p 2378-2380:2378-2380 -p 5671-5672:5671-5672 -p 15672:15672 -p 6379:6379 -p3306:3306 -v /opt/mysql:/var/lib/mysql -v /home/xy/luwak/svr:/root/svr xyzj/luwak-service:latest
+docker run -ti -p80:80 -p443:443 -p 8443:8443 -p 2377-2380:2377-2380 -p 5671-5672:5671-5672 -p 15672:15672 -p 6379:6379 -p3306:3306 -v /tmp:/tmp/ttt -v /home/xy/luwak/mysql:/var/lib/mysql -v /home/xy/Downloads/html/:/opt/html -v /home/xy/luwak/luwak-service/buildfiles/nginx/sites-enabled/:/etc/nginx/sites-enabled xyzj/luwak-service:latest
